851db6c19c52dcedc0c81284ed9fccbed79d7975
nickode <nicolasnsamaha@hotmail.com>
2020-01-19 19:54:06 -0500

    test

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
+
 @NgModule({
   imports: [
     IonicModule,
