287450c8e66bc23829256d5a69152b888c554993
MNezameddin <mnezam.31@gmail.com>
2020-02-28 19:48:36 -0500

    UC-123
    Fixed organization of methods used in map.ts

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -37,6 +37,27 @@ export class MapComponent implements AfterViewInit {
     });
   }
 
+//   NavigateMap(location:Location){  
+//     let mapOptions = {
+//       center: location,
+//       zoom: 17,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }
+
+//     this.map = new google.maps.Map(this.googleMap.nativeElement, mapOptions);
+//     var marker = new google.maps.Marker({
+//       position: location,
+//       map: this.map,
+//       title: 'Here'
+
+//   });
+// }
+
+NavigateMap(location:Location){  
+  this.marker.setPosition(new LatLng(location));
+
+}
+
   showMap(x:number){
 
     var mylocation = new google.maps.LatLng(this.userLocation.latitude,this.userLocation.longitude);
@@ -1865,22 +1886,6 @@ export class MapComponent implements AfterViewInit {
       });
     });  
     
-  }
-
-  NavigateMap(location:Location){  
-      let mapOptions = {
-        center: location,
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-
-      this.map = new google.maps.Map(this.googleMap.nativeElement, mapOptions);
-      var marker = new google.maps.Marker({
-        position: location,
-        map: this.map,
-        title: 'Here'
-
-    });
   }
 
   
