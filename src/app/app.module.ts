97c9cb00570fbcc5dc186d31eb58c4813670adef
AjithThanam <ajith.thanam@gmail.com>
2020-03-13 04:08:41 -0400

    Implemented google's direction service in new route component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
@@ -50,7 +51,7 @@ import { ToggleComponent } from './components/toggle/toggle.component';
                 ToggleComponent, LocateMeComponent, NearbyPointsOfInterestComponent, NewRouteComponent, AboutUsComponent, 
                 ReportIssuesComponent, SafetyComponent, ScheduleComponent, SettingsComponent, ShuttleBusScheduleComponent],
   entryComponents: [],
-  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
+  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule,FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/components/new-route/new-route.component.ts
+++ src/app/components/new-route/new-route.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+declare var google
 
 @Component({
   selector: 'app-new-route',
   templateUrl: './new-route.component.html',
   styleUrls: ['./new-route.component.scss'],
 })
-export class NewRouteComponent implements OnInit {
+
+
+export class NewRouteComponent  {
+  
+  directionsService = new google.maps.DirectionsService;
+  directionsRenderer = new google.maps.DirectionsRenderer;
+  directions = {}
 
   constructor() { }
 
-  ngOnInit() {}
+  setMap(){
+    var map = new google.maps.Map(document.getElementById('map'));
+    this.directionsRenderer.setMap(map);
+  }
+
+  getDirections() {
+
+    this.setMap();
+
+    this.directionsService.route({
+      origin: this.directions['start'],
+      destination: this.directions['destination'],
+      travelMode: 'WALKING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        console.log("directions Working")
+        this.directionsRenderer.setDirections(response);
+      } else {
+        window.alert('Request to directions API failed: ' + status);
+      }
+    });
+  }
+  
 
 }
