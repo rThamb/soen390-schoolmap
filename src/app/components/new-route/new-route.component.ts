7e6e2f33b93e1ddc71bcb91a91d6b749eb7dcc69
AjithThanam <ajith.thanam@gmail.com>
2020-03-14 01:17:19 -0400

    Added modes of tranportation selection

--- src/app/components/new-route/new-route.component.ts
+++ src/app/components/new-route/new-route.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component,ViewChild, ElementRef} from '@angular/core';
 
 declare var google
 
@@ -11,6 +11,7 @@ declare var google
 
 export class NewRouteComponent  {
   
+ 
   directionsService = new google.maps.DirectionsService;
   directionsRenderer = new google.maps.DirectionsRenderer;
   directions = {}
@@ -18,21 +19,60 @@ export class NewRouteComponent  {
   constructor() { }
 
   setMap(){
+    //creates an instance of the map to use in the backend, HOWEVER this create a new google map, need to change to use our own map
     var map = new google.maps.Map(document.getElementById('map'));
     this.directionsRenderer.setMap(map);
   }
 
-  getDirections() {
+  //Verifies is the user selected walking as their method of transportation
+  isWalking(): string{
+    var button = document.getElementById('walking');
+    if(button.classList.contains('segment-button-checked')) {
+      return "WALKING"
+    }
+  }
 
+  //Verifies is the user selected driving as their method of transportation
+  isDriving(): string{
+    var button = document.getElementById('driving');
+    if(button.classList.contains('segment-button-checked')) {
+      return "DRIVING"
+    }
+  }
+
+  //Verifies is the user selected wpublic transit as their method of transportation
+  isTransit(): string{
+    var button = document.getElementById('transit');
+    if(button.classList.contains('segment-button-checked')) {
+      return "TRANSIT"
+    }
+  }
+
+  //Determines which of the 3 available modes of transportation was selected
+  getTransportation():string{
+    if(this.isWalking() === "WALKING"){
+      return("WALKING")
+    }
+    else if (this.isTransit() === "TRANSIT"){
+      return("TRANSIT")
+    }
+    else{
+      return("DRIVING")
+    }
+  }
+
+  //Uses the google API to determin the route and draws the path on the map
+  getDirections() {
+    //this is a reference to the map
     this.setMap();
+    var travelMode = this.getTransportation()
 
     this.directionsService.route({
       origin: this.directions['start'],
       destination: this.directions['destination'],
-      travelMode: 'WALKING'
+      travelMode: travelMode
     }, (response, status) => {
       if (status === 'OK') {
-        console.log("directions Working")
         this.directionsRenderer.setDirections(response);
       } else {
         window.alert('Request to directions API failed: ' + status);
