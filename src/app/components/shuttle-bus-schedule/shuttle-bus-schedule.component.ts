d34fe3f69f648451fd627572bf42ecb5f29b939a
khalilalturk <khalil.alturk.ka@gmail.com>
2020-03-16 05:53:57 -0400

    UC-70 Was able to read the data from json file and export in tables, but NEED CSS Styling

--- src/app/components/shuttle-bus-schedule/shuttle-bus-schedule.component.ts
+++ src/app/components/shuttle-bus-schedule/shuttle-bus-schedule.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-shuttle-bus-schedule',
@@ -6,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shuttle-bus-schedule.component.scss'],
 })
 export class ShuttleBusScheduleComponent implements OnInit {
+ 
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+  arro: string [];
+  arrt: string [];
+  arrth: string [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.httpService.get('./assets/Shuttlebussched/schedule.json').subscribe(
+      data => {
+        this.arro = data as string [];  // FILL THE ARRAY WITH DATA.
+        console.log(this.arro);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      });
+      this.httpService.get('./assets/Shuttlebussched/scheduletwo.json').subscribe(
+        data => {
+          this.arrt = data as string [];  // FILL THE ARRAY WITH DATA.
+          console.log(this.arrt);
+        },
+        (err: HttpErrorResponse) => {
+          console.log (err.message);
+        });
+        this.httpService.get('./assets/Shuttlebussched/schedulethree.json').subscribe(
+          data => {
+            this.arrth = data as string [];  // FILL THE ARRAY WITH DATA.
+            console.log(this.arrth);
+          },
+          (err: HttpErrorResponse) => {
+            console.log (err.message);
+          });
+  }
+
 
 }
