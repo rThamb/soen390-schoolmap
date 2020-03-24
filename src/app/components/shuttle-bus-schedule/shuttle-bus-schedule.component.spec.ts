e3c58e68c2d15d78c14719c9dda5708514019354
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-03-24 19:52:57 -0400

    Update shuttle-bus-schedule.component.spec.ts
    UI testing shuttlebus component

--- src/app/components/shuttle-bus-schedule/shuttle-bus-schedule.component.spec.ts
+++ src/app/components/shuttle-bus-schedule/shuttle-bus-schedule.component.spec.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { ShuttleBusScheduleComponent } from './shuttle-bus-schedule.component';
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('ShuttleBusScheduleComponent', () => {
   let component: ShuttleBusScheduleComponent;
@@ -22,5 +23,15 @@ describe('ShuttleBusScheduleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.monday'));
+    expect(de.nativeElement.textContent).toContain('Monday');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.friday'));
+    expect(de.nativeElement.textContent).toContain('Friday');
   });
 });
