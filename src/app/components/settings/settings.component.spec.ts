c0e92150baf28ae12d2fa961d9220345fb4c9b3f
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-03-24 19:43:14 -0400

    Update settings.component.spec.ts
    UI testing for settings component

--- src/app/components/settings/settings.component.spec.ts
+++ src/app/components/settings/settings.component.spec.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 import { SettingsComponent } from './settings.component';
 import {NO_ERRORS_SCHEMA} from "@angular/core";
 import { IonicStorageModule } from '@ionic/storage';
+import {By} from "@angular/platform-browser";
 
 
 describe('SettingsComponent', () => {
@@ -26,5 +27,50 @@ describe('SettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Settings');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Language');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Preferences');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Accessibility');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.NestedList'));
+    expect(de.nativeElement.textContent).toContain('Elevators');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Stairs');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Escalators');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Calendar');
+  });
+
+  it('should contain a word describing the app', () => {
+    const de = fixture.debugElement.query(By.css('.settings'));
+    expect(de.nativeElement.textContent).toContain('Notifications');
   });
 });
