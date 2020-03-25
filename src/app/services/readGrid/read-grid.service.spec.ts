7556c65a26059a865019fb6bea4f045187005b6c
Taqi Haque <taqi_haque@hotmail.com>
2020-03-25 05:02:24 -0400

    fixed test for read-grid service

--- src/app/services/readGrid/read-grid.service.spec.ts
+++ src/app/services/readGrid/read-grid.service.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { Location } from '../../models/Location'
 import { Floor } from '../../models/Floor'
-
+import { FloorTile } from '../../models/FloorTile'
+import { IndoorPOI } from '../../models/IndoorPOI'
 import { ReadGridService } from './read-grid.service';
 
 describe('ReadGridService', () => {
@@ -14,46 +15,153 @@ describe('ReadGridService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should read json file and convert it to Floor object', async () => {
+  it('should read json file and convert it to a dictionnary that contains floor objects', async () => {
     const service: ReadGridService = TestBed.get(ReadGridService);
 
+    let expectedfloorsDictionnary = {};
     let expectedFloor = new Floor();
-    let topLeft = new Location(45.497163, -73.579545, 0);
+    let topLeft = new Location(45.497165, -73.579545, 0);
     let topRight = new Location(45.497709, -73.579038, 0);
-    let bottomLeft = new Location(45.496835, -73.578855, 0);
+    let bottomLeft = new Location(45.496829, -73.578850, 0);
     let bottomRight = new Location(45.497373, -73.578342, 0);
 
     expectedFloor.topLeftCornerGPS = topLeft; 
     expectedFloor.topRightCornerGPS = topRight;
     expectedFloor.bottomLeftCornerGPS = bottomLeft;
     expectedFloor.bottomRightCornerGPS = bottomRight;
-    // expectedFloor.pathfindingFloorGrid = [
-    //   [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-    //   [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-    //   [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-    //   [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-    //   [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-    //   [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-    //   [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-    //   [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1]
-    // ];
-    expectedFloor.pointsOfInterest = {"H840": {"x": 2,"y": 5 }, 
-                              "H890": { "x": 16,"y": 9} 
-                             };
 
-    let obtainedFloor: Floor = await service.createGrid('H8');
-    console.log(obtainedFloor);
-    expect(obtainedFloor).toEqual(expectedFloor);
+    expectedFloor.setFloorLevel(8);
+
+    let poi =
+    {
+      "HB840": {
+        "x": 2,
+        "y": 5,
+        "lat": 45.497386, 
+        "lng": -73.578535
+    }, 
+     "HB890": {
+        "x": 16,
+        "y": 9,
+        "lat": 45.497097,
+        "lng": -73.579132
+    },
+    "Washroom-Men": {
+        "x": 6, 
+        "y" : 15,
+        "lat": 0, 
+        "lng": 0
+    },
+    "Washroom-Women": {
+            "x": 9, 
+            "y": 3,
+            "lat": 0, 
+            "lng": 0  
+    },  
+    "Escalator-Up": {
+        "x": 9,
+        "y": 11,
+        "lat": 0, 
+        "lng": 0
+    }, 
+    "Escalator-Down": {
+        "x": 9,
+        "y": 6,
+        "lat": 0, 
+        "lng": 0
+    }, 
+    "Elevator" : {
+        "x": 12, 
+        "y": 11,
+        "lat": 0, 
+        "lng": 0
+    },       
+    "Stairs": [
+        {
+            "x": 4,
+            "y": 15,
+            "lat": 0, 
+            "lng": 0
+        },
+        {
+            "x": 4,
+            "y": 3,
+            "lat": 0, 
+            "lng": 0
+        } 
+    ]
+    }
+    expectedFloor.pointsOfInterest = poi;
+    
+    let binaryGrid= [
+      [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
+      [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
+      [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
+      [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
+      [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+      [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
+      [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
+      [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1]
+    ];
+    
+    let floorPlan: FloorTile[][] = [];
+    
+    
+    let length = binaryGrid.length;
+    let width = binaryGrid[0].length;
+    let arr = [];
+    for(let i = 0; i < length; i++){
+      arr = [];
+      let currentRow = binaryGrid[i];
+      for(let j = 0; j < width; j++){
+        let num = currentRow[j];
+        let tile = new FloorTile(null, num);
+        arr.push(tile);
+      }
+      floorPlan.push(arr);
+      arr = [];
+    }
+    expectedFloor.setFloorTileGrid(floorPlan); 
+
+
+    let keys = Object.keys(poi);
+    let pois: IndoorPOI[] = [];
+
+    for(let i =0; i<keys.length; i++){
+
+      let key = keys[i];
+
+      if(key != "Stairs"){
+        let curPoint = poi[key];
+        let obj = new IndoorPOI(curPoint["lat"], curPoint["lng"], curPoint["x"], curPoint["y"], key);
+        pois.push(obj);
+      }
+      else{
+        let stairsPois = poi[key];
+        for(let i =0; i < stairsPois.length; i++){
+          let curPoint = stairsPois[i];
+          let obj = new IndoorPOI(curPoint["lat"], curPoint["lng"], curPoint["x"], curPoint["y"], key);
+          pois.push(obj);
+        }    
+      }    
+    }
+
+    expectedFloor.setPois(pois);
+
+    expectedfloorsDictionnary["HB8"] = expectedFloor;
+    let obtainedFloorDictionnary: Floor = await service.createGrid('HB');
+    console.log(obtainedFloorDictionnary);
+    expect(obtainedFloorDictionnary["HB8"]).toEqual(expectedfloorsDictionnary["HB8"]);
   });
 });
