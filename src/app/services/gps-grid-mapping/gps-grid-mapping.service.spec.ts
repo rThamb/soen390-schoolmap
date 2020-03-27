f41a00ef3cd550db598a16fe0bbfe15f6c8e708e
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-03-26 20:44:55 -0400

    Revert "UI"
    This reverts commit c0475cf53933056189f8391f423e3b0f7191fe3c.

--- src/app/services/gps-grid-mapping/gps-grid-mapping.service.spec.ts
+++ src/app/services/gps-grid-mapping/gps-grid-mapping.service.spec.ts
@@ -1,48 +1,48 @@
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { TestBed } from '@angular/core/testing';
-import { Location } from '../../models/Location';
+import { Location } from '../../models/Location'
 import { Floor } from '../../models/Floor';
 import { GpsGridMappingService } from './gps-grid-mapping.service';
 
 describe('GpsGridMappingService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [GpsGridMappingService, Geolocation]
+    providers: [GpsGridMappingService,Geolocation]
   }).compileComponents());
 
   it('should be created', () => {
     const service: GpsGridMappingService = TestBed.get(GpsGridMappingService);
-    const result = service != null;
+    let result = service != null; 
     expect(service).toBeTruthy();
   });
 
-
-  it('should determine GPS and Coordinate',  () => {
-     const service: GpsGridMappingService = TestBed.get(GpsGridMappingService);
-
-     const coor = new Location(6.4, 4, 0);
-     const floor = new Floor();
-     const topLeft = new Location(4.4, 0, 0);
-     const topRight = new Location(12.4, 0, 0);
-     const bottomLeft = new Location(4.2, 6, 0);
-
-     const map = [];
-     const arr = [0, 0, 0, 0, 0];
-     map.push(arr);
-     map.push(arr);
-     map.push(arr);
-
-     floor.pathfindingFloorGrid = map;
-     floor.topLeftCornerGPS = topLeft;
-     floor.topRightCornerGPS = topRight;
-     floor.bottomLeftCornerGPS = bottomLeft;
-
-     const obtained = service.getFloorGridCoordinate(coor, floor);
-     console.log(obtained);
-     const expected = 1 + ',' + 2;
-     console.log(result);
-
-     expect(obtained + '').toContain(expected + '');
-   });
+  
+  // it('should determine GPS and Coordinate',  () => {
+  //   const service: GpsGridMappingService = TestBed.get(GpsGridMappingService);
+  //
+  //   let coor = new Location(6.4, 4, 0);
+  //   let floor = new Floor();
+  //   let topLeft = new Location(4.4, 0, 0);
+  //   let topRight = new Location(12.4, 0, 0);
+  //   let bottomLeft = new Location(4.2, 6, 0);
+  //
+  //   let map = [];
+  //   let arr = [0,0,0,0,0]
+  //   map.push(arr);
+  //   map.push(arr);
+  //   map.push(arr);
+  //
+  //   // floor.pathfindingFloorGrid = map;
+  //   // floor.topLeftCornerGPS = topLeft;
+  //   // floor.topRightCornerGPS = topRight;
+  //   // floor.bottomLeftCorrnerGPS = bottomLeft;
+  //
+  //   let obtained = service.getFloorGridCoordinate(coor, floor);
+  //   console.log(obtained);
+  //   let expected = 1 + "," + 2;
+  //   //console.log(result)
+  //
+  //   expect(obtained + "").toContain(expected + "");
+  // });
 
 
 });
