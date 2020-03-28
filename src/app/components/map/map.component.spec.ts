9a7d08a5288e159f5233599c174f9c9997871394
Taqi Haque <taqi_haque@hotmail.com>
2020-03-27 21:19:47 -0400

    Added few testing for map component

--- src/app/components/map/map.component.spec.ts
+++ src/app/components/map/map.component.spec.ts
@@ -3,34 +3,123 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { IndoorPathingService } from '../../services/indoorPathing/indoor-pathing.service';
 import { ReadGridService } from '../../services/readGrid/read-grid.service';
 import { IonicModule } from '@ionic/angular';
+import { BuildingFactoryService } from '../../services/BuildingFactory/building-factory.service';
 
 import { MapComponent } from './map.component';
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {By} from "@angular/platform-browser";
+import { MapService } from '../../services/map/map.service';
+
+declare var google;
 
 describe('MapComponent', () => {
-  let component: MapComponent;
-  let fixture: ComponentFixture<MapComponent>;
+  var component: MapComponent;
+  var fixture: ComponentFixture<MapComponent>;
+  var spy: any;
+  //let service: MapService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ MapComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [Geolocation, IndoorPathingService, ReadGridService],
+      providers: [Geolocation, IndoorPathingService, ReadGridService, BuildingFactoryService],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   }));
 
+  afterEach(() => {
+    //service = null;
+    //component = null;
+  });
+
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('should check if the class="map" is applied to map', async(() => {
+    fixture.detectChanges();
     const mapTag = fixture.debugElement.nativeElement.querySelector('div');
 
     // expect(mapTag.innerHTML).toContain('class="map"');
   }));
+
+  it('should create a marker and return it', async(() => 
+  {
+    fixture.detectChanges();
+    let map = component.map;
+    let locations = "";
+    let obtainedMarker = component.createMarker(locations, "Test");
+  
+    let expectedMarker = new google.maps.Marker
+    ({
+      position: locations,
+      map: map,
+      icon: "",
+      label: 
+      {
+          color: 'black',
+          fontWeight: 'bold',
+          text: "Test",
+          fontSize: '21px'
+      },
+    }); 
+    expect(obtainedMarker).toEqual(expectedMarker);
+  }));
+
+  it('should create a polygon and return it', async(() => 
+  {
+    fixture.detectChanges();
+    let map = component.map;
+
+    var testPath =
+    [
+      {lat: 45.497372, lng: -73.578338},
+      {lat: 45.496826, lng: -73.578859}
+    ];
+    
+    let obtainedPolygon = component.createPolygon("", "building");
+  
+    let expectedPolygon;
+    expectedPolygon = new google.maps.Polygon({
+      paths: "",
+      fillColor: "deepskyblue",
+    });
+    expectedPolygon.setMap(map);
+    expect(obtainedPolygon).toEqual(expectedPolygon);
+  }));
+
+//   it('method should be called', async (() => {
+//     spy = spyOn(component, "clearAllPOIMarkers").and.callThrough();
+//     expect(component).toBeDefined();
+//     expect(spy);
+//     expect(component.clearAllPOIMarkers).toHaveBeenCalled(); 
+// }));
+
+it('ngAfterViewInit should be called', () => {
+  spyOn(component, 'ngAfterViewInit'); 
+  fixture.detectChanges(); // trigger ngOnInit here
+
+  expect(component.ngAfterViewInit).toHaveBeenCalled(); 
 });
+
+it('should check that ion-content is loaded', async(() => {
+  let mapSearch= fixture.debugElement.query(By.css('ion-content'));
+
+  expect(mapSearch).toBeTruthy();
+}));
+
+// it("should call the 'loadBuilding' method on the 'BuildingFactoryService'", async (() => {
+//   let service: BuildingFactoryService;
+//   spy = spyOn(service, 'loadBuilding');
+//   fixture.detectChanges();
+//   component.enterBuilding("", "", "", false);
+//   expect(spy).toHaveBeenCalled();
+// }));
+
+});
+
--- src/app/services/readGrid/read-grid.service.spec.ts
+++ src/app/services/readGrid/read-grid.service.spec.ts
@@ -17,41 +17,50 @@ describe('ReadGridService', () => {
 
   // it('should read json file and convert it to a dictionnary that contains floor objects', async () => {
   //   const service: ReadGridService = TestBed.get(ReadGridService);
-  //
+  
   //   let expectedfloorsDictionnary = {};
   //   let expectedFloor = new Floor();
   //   let topLeft = new Location(45.497165, -73.579545, 0);
   //   let topRight = new Location(45.497709, -73.579038, 0);
   //   let bottomLeft = new Location(45.496829, -73.578850, 0);
   //   let bottomRight = new Location(45.497373, -73.578342, 0);
-  //
+  
   //   expectedFloor.topLeftCornerGPS = topLeft;
   //   expectedFloor.topRightCornerGPS = topRight;
   //   expectedFloor.bottomLeftCornerGPS = bottomLeft;
   //   expectedFloor.bottomRightCornerGPS = bottomRight;
-  //
+  
   //   expectedFloor.setFloorLevel(8);
-  //
+
+  
   //   let poi =
   //   {
   //     "HB840": {
-  //       "x": 2,
+  //       "x": 1,
   //       "y": 5,
   //       "lat": 45.497386,
   //       "lng": -73.578535
   //   },
+    
   //    "HB890": {
   //       "x": 16,
   //       "y": 9,
   //       "lat": 45.497097,
   //       "lng": -73.579132
-  //   },
+  //   },    
+  //   "HB832":{
+  //     "x": 7,
+  //     "y": 7,
+  //     "lat": 45.497234,
+  //     "lng": -73.579078
+  // },
   //   "Washroom-Men": {
   //       "x": 6,
   //       "y" : 15,
   //       "lat": 0,
   //       "lng": 0
   //   },
+
   //   "Washroom-Women": {
   //           "x": 9,
   //           "y": 3,
@@ -92,32 +101,35 @@ describe('ReadGridService', () => {
   //   ]
   //   }
   //   expectedFloor.pointsOfInterest = poi;
-  //
+  
   //   let binaryGrid= [
-  //     [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-  //     [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-  //     [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-  //     [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1],
-  //     [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-  //     [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-  //     [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1],
-  //     [1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1]
+  //     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1],
+  //     [1,1,1,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,0,1],
+  //     [1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1],
+  //     [1,1,1,0,1,1,0,0,1,1,1,0,0,1,1,1,1,1,0,1],
+  //     [1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  //     [1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,0,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1], 
+  //     [1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1], 
+  //     [1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1],
+  //     [1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1],
+  //     [1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
+  //     [1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  //     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
   //   ];
-  //
+  
+  //        //set dimensions
+  //        expectedFloor.setWidth(binaryGrid[0].length);
+  //        expectedFloor.setHeight(binaryGrid.length);
   //   let floorPlan: FloorTile[][] = [];
-  //
-  //
+  
+  
   //   let length = binaryGrid.length;
   //   let width = binaryGrid[0].length;
   //   let arr = [];
@@ -133,15 +145,15 @@ describe('ReadGridService', () => {
   //     arr = [];
   //   }
   //   expectedFloor.setFloorTileGrid(floorPlan);
-  //
-  //
+  
+  
   //   let keys = Object.keys(poi);
   //   let pois: IndoorPOI[] = [];
-  //
+  
   //   for(let i =0; i<keys.length; i++){
-  //
+  
   //     let key = keys[i];
-  //
+  
   //     if(key != "Stairs"){
   //       let curPoint = poi[key];
   //       let obj = new IndoorPOI(curPoint["lat"], curPoint["lng"], curPoint["x"], curPoint["y"], key);
@@ -156,9 +168,9 @@ describe('ReadGridService', () => {
   //       }
   //     }
   //   }
-  //
+  
   //   expectedFloor.setPois(pois);
-  //
+  
   //   expectedfloorsDictionnary["HB8"] = expectedFloor;
   //   let obtainedFloorDictionnary: Floor = await service.createGrid('HB');
   //   console.log(obtainedFloorDictionnary);
