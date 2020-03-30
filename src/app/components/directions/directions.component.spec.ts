c243e85b5c2a1eb47b43413ccc749f7321da829a
Nicolas Samaha <nicolasnsamaha@hotmail.com>
2020-03-29 21:57:42 -0400

    Fixed a test in Directions component

--- src/app/components/directions/directions.component.spec.ts
+++ src/app/components/directions/directions.component.spec.ts
@@ -5,13 +5,13 @@ import { GpsGridMappingService } from '../../services/gps-grid-mapping/gps-grid-
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import {Storage} from '@ionic/storage';
-import { IonicStorageModule } from '@ionic/storage'
+import { IonicStorageModule } from '@ionic/storage';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import {By} from '@angular/platform-browser';
 import { autoSpy } from '../../../../auto-spy';
 
 import { DirectionsComponent } from './directions.component';
-import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('DirectionsComponent', () => {
   let component: DirectionsComponent;
@@ -22,7 +22,7 @@ describe('DirectionsComponent', () => {
       declarations: [ DirectionsComponent ],
       schemas: [NO_ERRORS_SCHEMA],
       imports: [IonicModule.forRoot(),  IonicStorageModule.forRoot()],
-      providers:[Geolocation]
+      providers: [Geolocation]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DirectionsComponent);
@@ -34,42 +34,42 @@ describe('DirectionsComponent', () => {
      expect(component).toBeTruthy();
   });
 
-it('when isSGW is called it should verify string corresponds to SGW campus', () => {
-    expect(component.isSGW("concordia university")).toBeTruthy();
+  it('when isSGW is called it should verify string corresponds to SGW campus', () => {
+    expect(component.isSGW('concordia university')).toBeTruthy();
 });
 
-it('when isLoyola is called it should verify string corresponds to loyola', () => {
-    expect(component.isLoyola("concordia loyola")).toBeTruthy();
+  it('when isLoyola is called it should verify string corresponds to loyola', () => {
+    expect(component.isLoyola('concordia loyola')).toBeTruthy();
 });
 
-it('when isSchool is called it should check if is school', () => {
-    expect(component.isSchool("concordia")).toBeTruthy();
+  it('when isSchool is called it should check if is school', () => {
+    expect(component.isSchool('concordia')).toBeTruthy();
 });
 
-it('when displayShuttle is called it should check if shuttle should be displayed', () => {
-  component.directions['start'] = "concordia loyola";
-  component.directions['destination'] = "concordia";
-  let mockDate = new Date('2020-03-18 10:00')
-  jasmine.clock().mockDate(mockDate); 
+  it('when displayShuttle is called it should check if shuttle should be displayed', () => {
+  component.directions['start'] = 'concordia loyola';
+  component.directions['destination'] = 'concordia';
+  const mockDate = new Date('2020-03-18 10:00');
+  jasmine.clock().mockDate(mockDate);
   expect(component.displayShuttle()).toBeFalsy();
 });
 
-it('when isDriving is called it should verify if it is selected', () => {
+  it('when isDriving is called it should verify if it is selected', () => {
     expect(component.isDriving()).toBeFalsy();
 });
 
-it('when getTransportation is called it should get selected transportation', () => {
+  it('when getTransportation is called it should get selected transportation', () => {
     expect(component.getTransportation).toBeTruthy();
 });
 
-it('when displayTravelInfo is called it should display travel time and distance', () => {
-    var response =  { "routes": [ {
-        "legs": [ {
-            "duration": {
-                "text": "10 min"
+  it('when displayTravelInfo is called it should display travel time and distance', () => {
+    let response =  { 'routes': [ {
+        'legs': [ {
+            'duration': {
+                'text': '10 min'
             },
-            "distance": {
-                "text": "2 km"
+            'distance': {
+                'text': '2 km'
             }
         }]
     }]};
@@ -77,8 +77,8 @@ it('when displayTravelInfo is called it should display travel time and distance'
     expect(component.travelDistance).toContain('2 km');
 });
 
-it('when validateInput is called it should validate university address', () => {
-    expect(component.validateInput("Concordia University")).toBeTruthy();
+  it('when validateInput is called it should validate university address', () => {
+    expect(component.validateInput('Concordia University')).toBeTruthy();
 });
 /*
 it('when getDirections is called it should', () => {
@@ -111,17 +111,17 @@ it('when clearDirections is called it should', () => {
     // expect(c).toEqual
 });
 */
-it('when useCurrentLocation is called it should fill start with users location', async () => {
+  it('when useCurrentLocation is called it should fill start with users location', async () => {
     await component.useCurrentLocation().then( () => {
         expect(component.directions['start']).toContain(',');
     });
 
 });
 
-it('when getNextShuttleTime is called it should get the next shuttle time', async () => {
-    let mockDate = new Date('2020-03-18 10:00')
-    jasmine.clock().mockDate(mockDate); 
-    let result = await component.getNextShuttleTime("loyola");
+  it('when getNextShuttleTime is called it should get the next shuttle time', async () => {
+    const mockDate = new Date('2020-03-18 10:00');
+    jasmine.clock().mockDate(mockDate);
+    const result = await component.getNextShuttleTime('loyola');
     expect(result).toBeTruthy();
 });
 
