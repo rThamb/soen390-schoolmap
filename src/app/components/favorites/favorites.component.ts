635739d276079d64efec12a034355ea59f8c0d77
Dimitri <dimitrispy@gmail.com>
2020-04-07 17:46:00 -0400

    #57 Added ability to add and remove favorites

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'app-favorites',
@@ -7,8 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  favorite;
+  favorites;
 
-  ngOnInit() {}
+  constructor(private storage: Storage) { }
+
+  ngOnInit() {
+    this.getFavorites();
+  }
+
+  addFavorite(){
+    this.storage.get('favorites').then((val) => {
+      if(val){
+        let value = JSON.parse(val);
+        if(value.indexOf(this.favorite) == -1){
+          value.push(this.favorite);
+          this.storage.set('favorites', JSON.stringify(value));
+        }
+      }else{
+        let value = [this.favorite];
+        this.storage.set('favorites', JSON.stringify(value));
+      }
+      this.ngOnInit()
+    });
+  }
+
+  getFavorites(){
+    this.storage.get('favorites').then((val) => {
+      this.favorites = JSON.parse(val);
+    });
+  }
+
+  deleteFavorite(favoriteName: string){
+    if (confirm("Are you sure you would like remove this item from your favorites?") == true) {
+      this.storage.get('favorites').then((val) => {
+        if(val){
+          let value = JSON.parse(val);
+          let index = value.indexOf(favoriteName, 0);
+          if (index > -1) {
+            value.splice(index, 1);
+            this.storage.set('favorites', JSON.stringify(value));
+          }
+        }
+        this.ngOnInit()
+      });
+    }
+  }
 
 }
