beb57780fd3b4612e3f469d9e91809f4f7733d7a
nickode <nicolasnsamaha@hotmail.com>
2020-04-08 21:53:33 -0400

    Fixed a compiler error

--- src/app/components/my-calendar-events/my-calendar-events.component.ts
+++ src/app/components/my-calendar-events/my-calendar-events.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCalendarEventsComponent implements OnInit {
 
-  public today = "";
+  public today;
   constructor() { }
 
   ngOnInit() {
