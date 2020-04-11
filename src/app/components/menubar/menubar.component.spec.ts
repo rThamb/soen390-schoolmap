01c8518bf4f76ebd852bbab03ccb448f7178ae65
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-04-11 13:43:01 -0400

    Resolved 4 issues related to menubar component
    Resolves menubar component.

--- src/app/components/menubar/menubar.component.spec.ts
+++ src/app/components/menubar/menubar.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import {  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {RouterTestingModule} from '@angular/router/testing';
 import { MenubarComponent } from './menubar.component';
 import {By} from '@angular/platform-browser';
+import { IonicStorageModule } from '@ionic/storage';
 
-
-describe('MenubarComponent', () => {
+fdescribe('MenubarComponent', () => {
   let component: MenubarComponent;
   let fixture: ComponentFixture<MenubarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ MenubarComponent],
-      imports: [RouterTestingModule, IonicModule.forRoot()],
+      imports: [RouterTestingModule, IonicStorageModule.forRoot(), IonicModule.forRoot()],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
        providers: [IonicModule]
     }).compileComponents();
@@ -62,20 +62,26 @@ describe('MenubarComponent', () => {
     const button7 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(7)');
     button7.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/Settings');
-     });
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/EventOnCampus');
+      });
+
     const button8 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(8)');
     button8.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/Safety');
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/Settings');
      });
     const button9 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(9)');
     button9.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/ReportIssue');
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/Safety');
      });
     const button10 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(10)');
     button10.click();
+    fixture.whenStable().then(() => {
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/ReportIssue');
+     });
+    const button11 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(11)');
+    button11.click();
     fixture.whenStable().then(() => {
     expect(component.LoadNewPage).toHaveBeenCalledWith('/AboutUs');
    });
