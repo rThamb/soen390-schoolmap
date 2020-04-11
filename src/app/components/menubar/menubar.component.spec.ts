0ae07024c8f8a136cb6b9ef7089d53f27ec49040
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-04-11 16:58:13 -0400

    Update menubar.component.spec.ts
    resolved

--- src/app/components/menubar/menubar.component.spec.ts
+++ src/app/components/menubar/menubar.component.spec.ts
@@ -42,53 +42,64 @@ describe('MenubarComponent', () => {
     const button3 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(3)');
     button3.click();
     fixture.whenStable().then(() => {
-      expect(component.LoadNewPage).toHaveBeenCalledWith('/Favorites');
+      expect(component.LoadNewPage).toHaveBeenCalledWith('/NewRoute');
      });
     const button4 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(4)');
     button4.click();
     fixture.whenStable().then(() => {
-      expect(component.LoadNewPage).toHaveBeenCalledWith('/ShuttleBusSchedule');
+      expect(component.LoadNewPage).toHaveBeenCalledWith('/Favorites');
      });
     const button5 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(5)');
     button5.click();
     fixture.whenStable().then(() => {
-      expect(component.LoadNewPage).toHaveBeenCalledWith('/Schedule');
+      expect(component.LoadNewPage).toHaveBeenCalledWith('/ShuttleBusSchedule');
      });
     const button6 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(6)');
     button6.click();
     fixture.whenStable().then(() => {
-      expect(component.LoadNewPage).toHaveBeenCalledWith('/NearbyPointsOfInterest');
+      expect(component.LoadNewPage).toHaveBeenCalledWith('/Schedule');
      });
     const button7 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(7)');
     button7.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/EventOnCampus');
-      });
-
+      expect(component.LoadNewPage).toHaveBeenCalledWith('/NearbyPointsOfInterest');
+     });
     const button8 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(8)');
     button8.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/Settings');
-     });
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/EventOnCampus');
+      });
+
     const button9 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(9)');
     button9.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/Safety');
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/Settings');
      });
     const button10 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(10)');
     button10.click();
     fixture.whenStable().then(() => {
-       expect(component.LoadNewPage).toHaveBeenCalledWith('/ReportIssue');
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/Safety');
      });
     const button11 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(11)');
     button11.click();
+    fixture.whenStable().then(() => {
+       expect(component.LoadNewPage).toHaveBeenCalledWith('/ReportIssue');
+     });
+    const button12 = fixture.debugElement.nativeElement.querySelector('ion-item:nth-child(12)');
+    button12.click();
     fixture.whenStable().then(() => {
     expect(component.LoadNewPage).toHaveBeenCalledWith('/AboutUs');
    });
   }));
 
   it('should contain the name of the application', () => {
-    const de = fixture.debugElement.query(By.css('ion-header'));
+    const de = fixture.debugElement.query(By.css('ion-title:nth-child(2)'));
     expect(de.nativeElement.textContent).toContain('ConcordiaGo');
   });
+  it('should contain the menu title', () => {
+    const de = fixture.debugElement.query(By.css('ion-title:nth-child(1)'));
+    expect(de.nativeElement.textContent).toContain('Main Menu');
+  });
+
+
 });
