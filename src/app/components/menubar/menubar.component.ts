8bc95c7b882f2a5a6472ad770c9c0fd50aac00fe
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-04-11 12:41:10 -0400

    UC-55 + icons UI
    added link page of UC-55 related to events. And adjusted icons of menubar.

--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -29,7 +29,7 @@ export class MenubarComponent implements OnInit {
 
   // MenuBar Methods
   openFirst() {
-    
+
     this.menu.enable(true, 'first');
     this.menu.open('first');
   }
@@ -43,61 +43,57 @@ export class MenubarComponent implements OnInit {
     this.menu.open('custom');
   }
 
-  async languageSet()
-  {
-    console.log("languageSet called");
-    let res = await fetch("./assets/Languages/language.json");
-    let json = await res.json();
+  async languageSet() {
+    console.log('languageSet called');
+    const res = await fetch('./assets/Languages/language.json');
+    const json = await res.json();
 
-    //check if language is english with storage
+    // check if language is english with storage
     this.storage.ready().then(() => {
 
-      this.storage.get('languagePreference').then((lP)=> {
+      this.storage.get('languagePreference').then((lP) => {
 
       // If no setting has been set, default is english
-      if(lP == null)
-      {
+      if (lP == null) {
         lP = 'English';
         this.storage.set('languagePreference', 'English');
       }
-      if( lP === 'English')
-      {
-        document.getElementById("title").innerHTML = json.english.menubar.title;
-        document.getElementById("home").innerHTML = json.english.menubar.home;
-        document.getElementById("newroute").innerHTML = json.english.menubar.newRoute;
-        document.getElementById("favorites").innerHTML = json.english.menubar.favorites;
-        document.getElementById("shuttlebus").innerHTML = json.english.menubar.shuttleBusSchedule;
-        document.getElementById("schedule").innerHTML = json.english.menubar.mySchedule;
-        document.getElementById("nearbyPOI").innerHTML = json.english.menubar.nearbyPOI;
-        document.getElementById("settings").innerHTML = json.english.menubar.settings;
-        document.getElementById("safety").innerHTML = json.english.menubar.safety;
-        document.getElementById("report").innerHTML = json.english.menubar.reportAnIssue;
-        document.getElementById("aboutus").innerHTML = json.english.menubar.aboutUs;
-      }
-      //check if language is french with storage
-      else if (lP == 'French')
-      {
-        document.getElementById("title").innerHTML = json.french.menubar.title;
-        document.getElementById("home").innerHTML = json.french.menubar.home;
-        document.getElementById("newroute").innerHTML = json.french.menubar.newRoute;
-        document.getElementById("favorites").innerHTML = json.french.menubar.favorites;
-        document.getElementById("shuttlebus").innerHTML = json.french.menubar.shuttleBusSchedule;
-        document.getElementById("schedule").innerHTML = json.french.menubar.mySchedule;
-        document.getElementById("nearbyPOI").innerHTML = json.french.menubar.nearbyPOI;
-        document.getElementById("settings").innerHTML = json.french.menubar.settings;
-        document.getElementById("safety").innerHTML = json.french.menubar.safety;
-        document.getElementById("report").innerHTML = json.french.menubar.reportAnIssue;
-        document.getElementById("aboutus").innerHTML = json.french.menubar.aboutUs;
+      if ( lP === 'English') {
+        document.getElementById('title').innerHTML = json.english.menubar.title;
+        document.getElementById('home').innerHTML = json.english.menubar.home;
+        document.getElementById('newroute').innerHTML = json.english.menubar.newRoute;
+        document.getElementById('favorites').innerHTML = json.english.menubar.favorites;
+        document.getElementById('shuttlebus').innerHTML = json.english.menubar.shuttleBusSchedule;
+        document.getElementById('schedule').innerHTML = json.english.menubar.mySchedule;
+        document.getElementById('nearbyPOI').innerHTML = json.english.menubar.nearbyPOI;
+        document.getElementById('events').innerHTML = json.english.menubar.events;
+        document.getElementById('settings').innerHTML = json.english.menubar.settings;
+        document.getElementById('safety').innerHTML = json.english.menubar.safety;
+        document.getElementById('report').innerHTML = json.english.menubar.reportAnIssue;
+        document.getElementById('aboutus').innerHTML = json.english.menubar.aboutUs;
+      } else if (lP === 'French') {
+        document.getElementById('title').innerHTML = json.french.menubar.title;
+        document.getElementById('home').innerHTML = json.french.menubar.home;
+        document.getElementById('newroute').innerHTML = json.french.menubar.newRoute;
+        document.getElementById('favorites').innerHTML = json.french.menubar.favorites;
+        document.getElementById('shuttlebus').innerHTML = json.french.menubar.shuttleBusSchedule;
+        document.getElementById('schedule').innerHTML = json.french.menubar.mySchedule;
+        document.getElementById('nearbyPOI').innerHTML = json.french.menubar.nearbyPOI;
+        document.getElementById('events').innerHTML = json.french.menubar.events;
+        document.getElementById('settings').innerHTML = json.french.menubar.settings;
+        document.getElementById('safety').innerHTML = json.french.menubar.safety;
+        document.getElementById('report').innerHTML = json.french.menubar.reportAnIssue;
+        document.getElementById('aboutus').innerHTML = json.french.menubar.aboutUs;
       }
 
     });
 
-    })
+    });
   }
 
 
   ngOnInit() {
-    
+
   }
 
 }
