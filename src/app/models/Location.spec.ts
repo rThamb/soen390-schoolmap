7256d47ba0dd7ba706ead0ec5733cdc018df9fe7
zizo2310 <Moataz_fawzy@live.com>
2020-04-12 19:54:59 -0400

    adding tests for Location class

---
+++ src/app/models/Location.spec.ts
@@ -0,0 +1,38 @@
+import { Location} from './Location';
+
+fdescribe('Location', () => {
+it('when getLng is called it should', () => {
+    // arrange
+    const { build } = setup().default();
+    const c = build();
+    c.setLng(1);
+    const b = c.getLng();
+    // assert
+    expect(b).toEqual(1);
+});
+
+
+it('when getLat is called it should', () => {
+    // arrange
+    const { build } = setup().default();
+    const c = build();
+    // act
+    c.setLat(1);
+    const b = c.getLat();
+    // assert
+    expect(b).toEqual(1);
+});
+});
+
+function setup() {
+    const lat = 0 ;
+    const lng = 0;
+    const alt = 0;
+    const builder = {lat, lng, alt,
+        default() {return builder; },
+        build() {
+
+            return new Location(0, 0 , 0); }
+    };
+    return builder;
+}
