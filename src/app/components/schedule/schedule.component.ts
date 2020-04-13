e0929d1e00dea461a89ed9725803c44d2aebdd05
zizo2310 <Moataz_fawzy@live.com>
2020-04-12 22:32:35 -0400

    adding tests for ScheduleComponent class

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -16,28 +16,31 @@ export class ScheduleComponent implements OnInit {
   public email: string;
   public today;
 
-  constructor(private http: HttpClient, private storage: Storage, public navCtrl: NavController) 
-  {
+  constru;
+  private http: any;
+  private storage: any;
+  private navCtrl: any;
+  a: string;
+  ctor(http: HttpClient, storage: Storage, navCtrl: NavController) {
     this.today = Date.now();
     this.getNextEvents();
     this.translatePage();
   }
 
-  ngOnInit() 
-  {
+  ngOnInit() {
 
   }
-
+  setEvent(a) {
+    this.events = a;
+  }
   /**
    * Sends a request to the websever for retrieving upcoming calendar events from the gmail account
    */
-  getNextEvents()
-  {
+  getNextEvents() {
   this.http.get('http://concordiagocalendar.herokuapp.com/getNextEvents').subscribe(data => {
-  console.log(data);  
+  console.log(data);
 
-      if(data)
-      {
+  if (data) {
         console.log(data);
         this.events = data;
         this.email = this.events[0].creator.email;
@@ -46,28 +49,25 @@ export class ScheduleComponent implements OnInit {
           this.storage.set('events', this.events);
         });
 
-        for(var i = 0; i < this.events.length; i++){
-          if(this.events[i].start.dateTime || this.events[i].end.dateTime)
-          {
-            
+        for (let i = 0; i < this.events.length; i++) {
+          if (this.events[i].start.dateTime || this.events[i].end.dateTime) {
+
           }
 
         }
 
-      }
-      else{
+      } else {
         console.log('No events available');
       }
-      
-    })
+
+    });
   }
 
   /**
-   * When user clicks on an event, redirects them to the New Route page with the event's location set as the destination. 
+   * When user clicks on an event, redirects them to the New Route page with the event's location set as the destination.
    * @param location
    */
-  goToEventLocation(location)
-  {
+  goToEventLocation(location) {
     this.storage.ready().then(() => {
       this.storage.set('newRouteDest', location);
     });
@@ -78,58 +78,48 @@ export class ScheduleComponent implements OnInit {
 
   }
 
-  async translatePage()
-  {
-    let res = await fetch("./assets/Languages/language.json");
-    let json = await res.json();
+  async translatePage() {
+    const res = await fetch('./assets/Languages/language.json');
+    const json = await res.json();
 
-    //check if language is english with storage
+    // check if language is english with storage
     this.storage.ready().then(() => {
 
-      this.storage.get('languagePreference').then((lP)=> {
+      this.storage.get('languagePreference').then((lP) => {
 
       // If no setting has been set, default is english
-      if(lP == null)
-      {
+      if (lP == null) {
         lP = 'English';
         this.storage.set('languagePreference', 'English');
       }
-      if( lP === 'English')
-      {
-        document.getElementById("email").innerHTML = json.english.schedule.email;
-        document.getElementById("date").innerHTML = json.english.schedule.date;
-        document.getElementById("upcoming").innerHTML = json.english.schedule.event;
-        for (var i=0; i < document.getElementsByClassName("eventStart").length; i++)
-        {
-          document.getElementsByClassName("eventStart")[i].innerHTML = json.english.schedule.start;
+      if ( lP === 'English') {
+        document.getElementById('email').innerHTML = json.english.schedule.email;
+        document.getElementById('date').innerHTML = json.english.schedule.date;
+        document.getElementById('upcoming').innerHTML = json.english.schedule.event;
+        for (let i = 0; i < document.getElementsByClassName('eventStart').length; i++) {
+          document.getElementsByClassName('eventStart')[i].innerHTML = json.english.schedule.start;
         }
-        for (var i=0; i < document.getElementsByClassName("eventEnd").length; i++)
-        {
-          document.getElementsByClassName("eventEnd")[i].innerHTML = json.english.schedule.end;
+        for (let i = 0; i < document.getElementsByClassName('eventEnd').length; i++) {
+          document.getElementsByClassName('eventEnd')[i].innerHTML = json.english.schedule.end;
         }
-      }
-      //check if language is french with storage
-      else if (lP == 'French')
-      {
-        document.getElementById("email").innerHTML = json.french.schedule.email;
-        document.getElementById("date").innerHTML = json.french.schedule.date;
-        document.getElementById("upcoming").innerHTML = json.french.schedule.event;
-        console.log(document.getElementsByClassName("eventStart").length);
-        var len = document.getElementsByClassName("eventStart");
+      } else if (lP === 'French') {
+        document.getElementById('email').innerHTML = json.french.schedule.email;
+        document.getElementById('date').innerHTML = json.french.schedule.date;
+        document.getElementById('upcoming').innerHTML = json.french.schedule.event;
+        console.log(document.getElementsByClassName('eventStart').length);
+        const len = document.getElementsByClassName('eventStart');
         console.log(len.length);
-        for (var i=0; i < document.getElementsByClassName("eventStart").length; i++)
-        {
-          document.getElementsByClassName("eventStart")[i].innerHTML = json.french.schedule.start;
+        for (let i = 0; i < document.getElementsByClassName('eventStart').length; i++) {
+          document.getElementsByClassName('eventStart')[i].innerHTML = json.french.schedule.start;
         }
-        for (var i=0; i < document.getElementsByClassName("eventEnd").length; i++)
-        {
-          document.getElementsByClassName("eventEnd")[i].innerHTML = json.french.schedule.end;
+        for (let i = 0; i < document.getElementsByClassName('eventEnd').length; i++) {
+          document.getElementsByClassName('eventEnd')[i].innerHTML = json.french.schedule.end;
         }
       }
 
     });
 
-    })
+    });
   }
 
 }
