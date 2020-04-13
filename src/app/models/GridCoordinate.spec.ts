7247cbfc7c39ba9c4984f833eb6ebad83bdc50ce
zizo2310 <Moataz_fawzy@live.com>
2020-04-13 18:20:08 -0400

    new GridCoordinate.spec.ts

---
+++ src/app/models/GridCoordinate.spec.ts
@@ -0,0 +1,26 @@
+
+import {GridCoordinate} from './GridCoordinate';
+fdescribe('GridCoordinate', () => {
+it('should create GridCoordinate', () => {
+    const { build } = setup().default();
+    const c = build();
+    const b = new GridCoordinate(0, 0);
+    expect(c).toEqual(b);
+});
+});
+
+function setup() {
+    const x = 0 ;
+    const y = 0;
+    const builder = {
+        x,
+        y,
+        default() {
+            return builder;
+        },
+        build() {
+            return new GridCoordinate(x, y);
+        }
+    };
+    return builder;
+}
