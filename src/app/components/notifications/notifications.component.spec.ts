b21997a8b94fde463da0ca6a55c8714c800c8667
JenniferAbouJaoudi <43418483+JenniferAbouJaoudi@users.noreply.github.com>
2020-04-12 20:00:26 -0400

    Update notifications.component.spec.ts

--- src/app/components/notifications/notifications.component.spec.ts
+++ src/app/components/notifications/notifications.component.spec.ts
@@ -1,25 +1,101 @@
+import { NavController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import {LocalNotifications} from '@ionic-native/local-notifications/ngx';
 import { NotificationsComponent } from './notifications.component';
+import { autoSpy } from 'auto-spy';
+import {By} from '@angular/platform-browser';
 
 describe('NotificationsComponent', () => {
   let component: NotificationsComponent;
   let fixture: ComponentFixture<NotificationsComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+const a = setup().default();
+TestBed.configureTestingModule({
       declarations: [ NotificationsComponent ],
       imports: [IonicModule.forRoot()],
       providers: [ LocalNotifications]
-    }).compileComponents();
+    }).configureTestingModule({ providers: [{ provide: NavController, useValue: a.navCtrl },
+            { provide: Platform, useValue: a.plt },
+            { provide: AlertController, useValue: a.alertCtrl },
+            { provide: HttpClient, useValue: a.http },
+            { provide: Storage, useValue: a.storage }] }).compileComponents();
 
-    fixture = TestBed.createComponent(NotificationsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+fixture = TestBed.createComponent(NotificationsComponent);
+component = fixture.componentInstance;
+fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain a the able/disable text', () => {
+  const de = fixture.debugElement.query(By.css('.lines'));
+  expect(de.nativeElement.textContent).toContain('Allow notifications');
+
+  });
+
+  it('when Clicked is called it should', () => {
+    // arrange
+    const { build } = setup().default();
+    const c = build();
+    // act
+    c.Clicked();
+    // assert
+    // tslint:disable-next-line:no-unused-expression
+    expect(c).toBeTruthy;
 });
+
+  it('when refreshEvents is called it should', () => {
+    // arrange
+    const { build } = setup().default();
+    const c = build();
+    // act
+    c.refreshEvents();
+    // assert
+    // tslint:disable-next-line:no-unused-expression
+    expect(c).toBeTruthy;
+});
+
+  it('when ngOnInit is called it should', () => {
+    // arrange
+    const { build } = setup().default();
+    const c = build();
+    //act
+    c.ngOnInit();
+   // assert
+    // tslint:disable-next-line:no-unused-expression
+    expect(c).toBeTruthy;
+});
+
+})
+
+function setup() {
+    const navCtrl = autoSpy(NavController);
+    const plt = autoSpy(Platform);
+    const localNotification = autoSpy(LocalNotifications);
+    const alertCtrl = autoSpy(AlertController);
+    const http = autoSpy(HttpClient);
+    const storage = autoSpy(Storage);
+    const builder = {
+        navCtrl,
+        plt,
+        localNotification,
+        alertCtrl,
+        http,
+        storage,
+        default() {
+            return builder;
+        },
+        build() {
+            return new NotificationsComponent(navCtrl, plt, localNotification, alertCtrl, http, storage);
+        }
+    };
+    return builder;
+}
